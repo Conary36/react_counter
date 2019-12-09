3c1b95572a76068b6b79d546c120ba9394a76b10
Conary36 <cbeckford144@gmail.com>
2019-12-09 12:45:26 -0500

    Theme Context and button styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { Fragment } from 'react';
-// import logo from './logo.svg';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 import Counter from './Counter';
 import CounterHooks from './CounterHooks';
 
+export const ThemeContext = React.createContext()
+
 function App() {
   console.log('Render App');
+  const [theme, setTheme]= useState('red')
   return (
+    <ThemeContext.Provider value={{backgroundColor: theme}}>
     <Fragment>Fragments are used to return multiple items
       <Counter initialCount={0}/>Counter
       <CounterHooks initialCount={0}/>Counter Hooks
     </Fragment>
+    <button onClick={()=> setTheme(prevTheme =>{
+      return prevTheme === 'red' ? 'blue': 'red';
+    })}>Toggle Theme</button>
+    </ThemeContext.Provider>
   )
 }
 
